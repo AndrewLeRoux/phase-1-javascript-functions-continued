ecfbb93400d9b31c853754dafb010cbfbb7c569a
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-11-30 09:54:05 -0800

    commit tree ab0c0c64877c06670f47d7c5653fcf92a9d761e9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// saturdayFun function
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// mondayWork function
+const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+// wrapAdjective function
+function wrapAdjective (visualFlair = '*') {
+    return function (adjective = 'special') {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+}
